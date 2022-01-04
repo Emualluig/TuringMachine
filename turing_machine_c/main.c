0f45ec620f50adbb1fc2e044631f0a1ce1c9ac5f
Emualluig <Emualluig@users.noreply.github.com>
2022-01-03 23:02:22 -0400

    C project

---
+++ turing_machine_c/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+struct TuringNode {
+
+	struct TuringNode* connections[256];
+};
+
+struct TuringNode create_turingnode() {
+
+}
+
+int main() {
+	printf("hello world");
+	return 0;
+};
